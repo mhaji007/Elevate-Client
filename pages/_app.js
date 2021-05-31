d35b6f90243ad065fc755e00d3aebaabc9d415bb
Mehdi Hajikhani <mhaji007@fiu.edu>
2021-05-31 14:54:45 -0400

    Set up registration form

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,21 @@ import TopNav from "../components/TopNav";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "antd/dist/antd.css";
 import "../public/css/styles.css";
+import useIsClient from "../utils/useIsClient"
 
 function MyApp({ Component, pageProps }) {
+  const isClient = useIsClient();
   return (
     <>
-      <TopNav />
-
-      <Component {...pageProps} />
+      {isClient && (
+        <>
+          <TopNav />
+          <Component {...pageProps} />
+        </>
+      )}
     </>
   );
+
 }
 
 export default MyApp;
--- pages/register.js
+++ pages/register.js
@@ -1,12 +1,56 @@
-import React from "react";
+import { useState } from "react";
 
 function Register() {
   // Jumbotron class has been removed in bootstrap 5
 
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.table({ name, email, password });
+  };
+
   return (
-    <div className="jumbotron h-100 p-5  text-center bg-primary square">
-      <h1>register</h1>
-    </div>
+    <>
+      <h1 className="jumbotron h-100 p-5  text-center bg-primary square">
+        Register
+      </h1>
+      <div className="container col-md-4 offset-md-4 pb-5">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="form-control mb-4 p-3"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Enter name"
+            required
+          />
+          <input
+            type="email"
+            className="form-control mb-4 p-3"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter email"
+            required
+          />
+          <input
+            type="password"
+            className="form-control mb-4 p-3"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter password"
+            required
+          />
+          <div class="d-grid gap-2">
+            <button type="submit" className="btn btn-primary p-2">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
 
