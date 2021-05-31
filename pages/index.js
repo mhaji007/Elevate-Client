887b2738825130a33aacb495d0de20fa4b4776c3
Mehdi Hajikhani <mhaji007@fiu.edu>
2021-05-30 20:31:19 -0400

    Set up custom css

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import React from "react";
 function Index() {
   return (
     <>
-      <h1 className="jumbotron text-center">Elevate</h1>
+      <h1 className="jumbotron text-center square">Elevate</h1>
     </>
   );
 }
