8fd0d0e86109c7c070a1a0f7d8a924555f6cdd9b
Mehdi Hajikhani <mhaji007@fiu.edu>
2021-06-04 15:36:32 -0400

    Implement logout functionality

--- components/TopNav.js
+++ components/TopNav.js
@@ -1,11 +1,17 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { Menu } from "antd";
 import Link from "next/link";
 import {
   AppstoreOutlined,
   LoginOutlined,
+  LogoutOutlined,
   UserAddOutlined,
 } from "@ant-design/icons";
+
+import { Context } from "../context";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { toast } from "react-toastify";
 
 const { Item } = Menu;
 
@@ -13,10 +19,22 @@ function topNav() {
   // Keep track of page user is on at any moment
   const [current, setCurrent] = useState("");
 
+  const { state, dispatch } = useContext(Context);
+
+  const router = useRouter();
+
   useEffect(() => {
     // process.browser is deprecated
     typeof window !== "undefined" && setCurrent(window.location.pathname);
   }, [typeof window !== "undefined" && window.location.pathname]);
+
+  const logout = async () => {
+    dispatch({ type: "LOGOUT" });
+    window.localStorage.removeItem("user");
+    const response = await axios.get("/api/logout");
+    toast.success(response.data.message);
+    router.push("/login");
+  };
 
   return (
     <Menu mode="horizontal" selectedKeys={[current]}>
@@ -46,6 +64,9 @@ function topNav() {
         <Link href="/register">
           <a>Register</a>
         </Link>
+      </Item>
+      <Item onClick={logout} icon={<LogoutOutlined />} className="">
+        Logout
       </Item>
     </Menu>
   );
